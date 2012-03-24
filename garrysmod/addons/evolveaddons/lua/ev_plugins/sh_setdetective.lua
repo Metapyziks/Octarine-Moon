4360e1a9f2d25008d5067a694e51f5b2948d5dff
Jack Lawton <lawton27@gmail.com>
2012-03-24 22:18:05 +0000

    fixed typos and added random feature if no player is specified

--- garrysmod/addons/evolveaddons/lua/ev_plugins/sh_setdetective.lua
+++ garrysmod/addons/evolveaddons/lua/ev_plugins/sh_setdetective.lua
@@ -7,29 +7,56 @@ PLUGIN.Usage = "[players]"
 PLUGIN.Privileges = { "Promote To Detective" }
 
 function PLUGIN:Call( ply, args )
-	if ( ply:EV_HasPrivilege( "Promote To Detective" ) ) then
+
+	
+	if ( ply:EV_HasPrivilege( "Promote To Detective" ) and GetRoundState() == ROUND_ACTIVE ) then
 		local players = evolve:FindPlayer( args, ply )
 
-		for _, pl in ipairs( players ) do
-			if(pl:IsTraitor()) then
-				evolve:Notify( ply, evolve.colors.red, "Cannot use this command on a traitor!" )
-			elseif(ply:IsActiveDetective()) then
-				evolve:notify( ply, evolve.colors.blue, pl:Nick(), evolve.color.red, "is already a Detective!" )
-			else
-				pl:SetRole(ROLE_DETECTIVE)
-				SendFullStateUpdate() -- See terrortown/gamemode/traitor_state.lua This sends the new status to all clients
-				if( not pl:HasWeapon( "weapon_ttt_wtester" ) ) then
-					pl:Give( "weapon_ttt_wtester" ) -- only gives player scanner if they don't already have one
+		if ( #players > 0 ) then -- we have players to promote!
+			
+		else -- otherwise we'll have to find someone...
+			-- first define some tables for the players we shall pick at random from
+			local firstchoices = {} -- these players want to be detective
+			local secondchoices = {} -- these players don't want to be detective but might have to be
+			
+			for k,v in pairs(player.GetAll()) do -- for all players
+				-- first let's check that the person is alive, valid and not a traitor or detective
+				if( v:IsValid() and v:IsTerror() and v:Alive() and not v:IsActiveDetective() and not v:IsRole(ROLE_TRAITOR)) then
+					if v:GetAvoidDetective() then
+						table.insert(secondchoices, v) -- if they want to be a second choice
+					else
+						table.insert(firstchoices, v) -- everyone else
+					end
 				end
-				pl:AddCredits( 1 )
-				evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has promoted ", evolve.colors.blue, pl:Nick(), evolve.colors.white, " to detective!" )
+			end
+			-- now we need to pick someone at random
+			if( #firstchoices > 0 ) then -- people who want to be detective exist
+				table.insert(players, table.Random( firstchoices )
+			elseif( #firstchoices > 0 ) then  -- otherwise pick a second choice
+				table.insert(players, table.Random( secondchoices )
+			else
+				evolve:notify( ply, evolve.colors.red, "Unable to find suitable player to promote." )
 			end
 		end
 
-		if ( #players > 0 ) then
-			
-		else
-			evolve:Notify( ply, evolve.colors.red, evolve.constants.noplayers )
+		for _, pl in ipairs( players ) do
+			if(pl:IsValid() and pl:IsTerror() and v:Alive()) then
+				if(pl:IsTraitor()) then
+					evolve:Notify( ply, evolve.colors.red, "Cannot use this command on a traitor!" )
+				elseif(pl:IsActiveDetective()) then
+					evolve:notify( ply, evolve.colors.blue, pl:Nick(), evolve.colors.red, "is already a Detective!" )
+				else
+					pl:SetRole(ROLE_DETECTIVE)
+					SendFullStateUpdate() -- See terrortown/gamemode/traitor_state.lua This sends the new status to all clients
+					if( not pl:HasWeapon( "weapon_ttt_wtester" ) ) then
+						pl:Give( "weapon_ttt_wtester" ) -- only gives player scanner if they don't already have one
+					end
+					pl:AddCredits( 1 )
+					evolve:Notify( evolve.colors.blue, pl:Nick(), evolve.colors.white, " has been promoted to detective!" )
+				end
+			else
+				evolve:Notify( ply, evolve.colors.red, "Unnable to promote player" )
+			end
 		end
 	else
 		evolve:Notify( ply, evolve.colors.red, evolve.constants.notallowed )
