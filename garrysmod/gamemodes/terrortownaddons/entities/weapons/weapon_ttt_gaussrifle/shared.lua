2c4e1c03ada182c872d61cb58180eaf0e2527996
Metapyziks <metapyziks@gmail.com>
2012-03-27 22:06:39 +0100

    Gauss rifle now has aim assisting

--- garrysmod/gamemodes/terrortownaddons/entities/weapons/weapon_ttt_gaussrifle/shared.lua
+++ garrysmod/gamemodes/terrortownaddons/entities/weapons/weapon_ttt_gaussrifle/shared.lua
@@ -1,22 +1,22 @@
 
 if SERVER then
-   AddCSLuaFile( "shared.lua" )
-   resource.AddFile("materials/VGUI/ttt/icon_gauss.vmt")
+	AddCSLuaFile( "shared.lua" )
+	resource.AddFile("materials/VGUI/ttt/icon_gauss.vmt")
 end
 
 SWEP.HoldType           = "ar2"
 
 if CLIENT then
-   SWEP.PrintName          = "Gauss Rifle"
+	SWEP.PrintName          = "Gauss Rifle"
 
-   SWEP.Slot               = 7
+	SWEP.Slot               = 7
 
-   SWEP.Icon = "VGUI/ttt/icon_gauss"
-   
-   SWEP.EquipMenuData = {
-      type = "Weapon",
-      desc = "High power, high accuracy\nGauss rifle which leaves a\ntracer after each shot.\nCan only be shot when zoomed\nin, and has limited ammo."
-   };
+	SWEP.Icon = "VGUI/ttt/icon_gauss"
+
+	SWEP.EquipMenuData = {
+		type = "Weapon",
+		desc = "High power, high accuracy\nGauss rifle which leaves a\ntracer after each shot.\nCan only be shot when zoomed\nin, and has limited ammo."
+	};
 end
 
 
@@ -56,15 +56,15 @@ SWEP.IronSightsPos      = Vector( 5, -15, -2 )
 SWEP.IronSightsAng      = Vector( 2.6, 1.37, 3.5 )
 
 function SWEP:SetZoom(state)
-    if CLIENT then 
-       return
-    else
-       if state then
-          self.Owner:SetFOV(20, 0.3)
-       else
-          self.Owner:SetFOV(0, 0.2)
-       end
-    end
+	if CLIENT then 
+		return
+	else
+		if state then
+			self.Owner:SetFOV(20, 0.3)
+		else
+			self.Owner:SetFOV(0, 0.2)
+		end
+	end
 end
 
 function SWEP:ShootBullet( dmg, recoil, numbul, cone )
@@ -79,12 +79,24 @@ function SWEP:ShootBullet( dmg, recoil, numbul, cone )
 	local sights = self:GetIronsights()
 
 	numbul = numbul or 1
-	cone   = cone   or 0.01
+	cone   = cone   or 0
 
+	self.Owner:LagCompensation(true)
 	local bullet = {}
 	bullet.Num    = numbul
 	bullet.Src    = self.Owner:GetShootPos()
 	bullet.Dir    = self.Owner:GetAimVector()
+	if self:GetNWBool( "HasTarget" ) then
+		local targ = self:GetNWEntity( "CurTarget" )
+		if IsValid( targ ) then
+			local pos = targ:GetPos() + Vector( 0, 0, 60 )
+			if targ:Crouching() then
+				pos = pos + Vector( 0, 0, -36 )
+			end
+			local diff = pos - self.Owner:GetShootPos()
+			bullet.Dir = diff:GetNormal()
+		end
+	end
 	bullet.Spread = Vector( cone, cone, 0 )
 	bullet.Tracer = 4
 	bullet.Force  = 10
@@ -110,8 +122,9 @@ function SWEP:ShootBullet( dmg, recoil, numbul, cone )
 			ent:SetTeleportMark( pos, ang )
 		end
 	end
-
+	
 	self.Owner:FireBullets( bullet )
+	self.Owner:LagCompensation(false)
 
 	-- Owner can die after firebullets
 	if (not IsValid(self.Owner)) or (not self.Owner:Alive()) or self.Owner:IsNPC() then return end
@@ -173,8 +186,10 @@ function SWEP:SecondaryAttack()
     
     if SERVER then
         self:SetZoom(bIronsights)
-     else
+		self:SetNWBool( "HasTarget", false )
+    else
         self:EmitSound(self.Secondary.Sound)
+		self.CurTarget = nil
     end
     
     self.Weapon:SetNextSecondaryFire( CurTime() + 0.3 )
@@ -192,84 +207,225 @@ function SWEP:Reload()
     self:SetZoom(false)
 end
 
-
 function SWEP:Holster()
     self:SetIronsights(false)
     self:SetZoom(false)
     return true
 end
 
+if SERVER then
+	function playerChangeTarget( ply, cmd, args )
+		if IsValid( ply ) and ply:Alive() then
+			local wep = ply:GetActiveWeapon()
+			if IsValid( wep ) and wep:GetClass() == "weapon_ttt_gaussrifle" then
+				if not tonumber( args[ 1 ] ) or tonumber( args[ 1 ] ) == -1 then
+					wep:SetNWBool( "HasTarget", false )
+				else
+					local targ = Entity( args[ 1 ] )
+					if IsValid( targ ) and targ:IsPlayer() and targ:IsTerror() and targ:Alive() then
+						wep:SetNWBool( "HasTarget", true )
+						wep:SetNWEntity( "CurTarget", targ )
+					end
+				end
+			end
+		end
+	end
+	concommand.Add( "ttt_gauss_changetarget", playerChangeTarget )
+end
+
 if CLIENT then
-   local scope = surface.GetTextureID("sprites/scope")
-   function SWEP:DrawHUD()
-      if self:GetIronsights() then
-         surface.SetDrawColor( 0, 0, 0, 255 )
-         
-         local x = ScrW() / 2.0
-         local y = ScrH() / 2.0
-         local scope_size = ScrH()
-
-         -- crosshair
-         local gap = 80
-         local length = scope_size
-         surface.DrawLine( x - length, y, x - gap, y )
-         surface.DrawLine( x + length, y, x + gap, y )
-         surface.DrawLine( x, y - length, x, y - gap )
-         surface.DrawLine( x, y + length, x, y + gap )
-
-         gap = 0
-         length = 50
-         surface.DrawLine( x - length, y, x - gap, y )
-         surface.DrawLine( x + length, y, x + gap, y )
-         surface.DrawLine( x, y - length, x, y - gap )
-         surface.DrawLine( x, y + length, x, y + gap )
-
-
-         -- cover edges
-         local sh = scope_size / 2
-         local w = (x - sh) + 2
-         surface.DrawRect(0, 0, w, scope_size)
-         surface.DrawRect(x + sh - 2, 0, w, scope_size)
-
-         surface.SetDrawColor(255, 0, 0, 255)
-         surface.DrawLine(x, y, x + 1, y + 1)
-
-         -- scope
-         surface.SetTexture(scope)
-         surface.SetDrawColor(255, 255, 255, 255)
-
-         surface.DrawTexturedRectRotated(x, y, scope_size, scope_size, 0)
-		 
-		 surface.SetDrawColor(0, 0, 0, 255)
-		 surface.DrawRect( ScrW() / 2 - 129, ScrH() * 3 / 4 - 9, 258, 18 )
-		 if self:Clip1() ~= 0 then
-			local ctime = CurTime()
-			local r = 255
-			local gb = 0
-			if ctime >= self.ChargeEnd then
-			   local wave = ( math.sin( ( CurTime() - self.ChargeEnd ) * math.pi * 2 ) * 0.5 + 0.5 )
-			   r = wave * r + ( 1 - wave ) * 255
-			   gb = wave * gb + ( 1 - wave ) * 255
+	SWEP.LastTargCheck = 0
+	SWEP.Targets = {}
+	SWEP.CurTarget = nil
+	
+	local scope = surface.GetTextureID("sprites/scope")
+	function SWEP:CheckForTargets()
+		self.LastTargCheck = CurTime()
+		
+		local lply = LocalPlayer()
+		local aim = lply:GetAimVector()
+		
+		local fov = 10 / 180 * math.pi
+		
+		self.Targets = {}
+		
+		for _, ply in pairs( player.GetAll() ) do
+			if ply ~= lply and IsValid( ply ) and ply:Alive() and ply:IsTerror() then
+				local pos = ply:GetPos() + Vector( 0, 0, 60 )
+				if ply:Crouching() then
+					pos = pos + Vector( 0, 0, -36 )
+				end
+				local diff = pos - lply:GetShootPos()
+				local norm = diff:GetNormal()
+				local ang = math.acos( aim:Dot( norm ) )
+				
+				if ang < fov and diff:Length() > 128 then
+					local trdata = {}
+					trdata.start = lply:GetShootPos()
+					trdata.endpos = pos
+					trdata.filter = lply
+					trdata.mask = MASK_SHOT
+					local tr = util.TraceLine( trdata )
+		
+					if not tr.Hit or tr.Entity == ply or ( IsValid( tr.Entity ) and tr.Entity:GetParent() == ply ) or tr.HitPos:Distance( pos ) <= 16 then
+						table.insert( self.Targets, { Angle = ang, Player = ply } )
+					end
+				end
 			end
-			surface.SetDrawColor(r, gb, gb, 255)
-			local prog = math.max( math.min( ( CurTime() - self.ChargeEnd + self.ChargeTime ) / self.ChargeTime, 1 ), 0 )
-			local len = math.floor( prog * 256 )
-			surface.DrawRect( ScrW() / 2 - 128, ScrH() * 3 / 4 - 8, len, 16 )
+		end
+		
+		table.sort( self.Targets, function( a, b )
+			return a.Angle < b.Angle
+		end )
+	end
+	
+	function SWEP:UpdateTarget( targ )
+		self.CurTarget = targ
+		if IsValid( targ ) then
+			RunConsoleCommand( "ttt_gauss_changetarget", targ:EntIndex() )
+		else
+			RunConsoleCommand( "ttt_gauss_changetarget", -1 )
+		end			
+	end
+
+	function SWEP:DrawHUD()
+		if self:GetIronsights() then
+			if CurTime() - self.LastTargCheck > 0.25 then
+				self:CheckForTargets()
+			end
 			
-			local text = "" .. math.floor( prog * 100 ) .. "%"
-			if prog == 1 then
-				text = "FULLY CHARGED"
+			surface.SetDrawColor( 0, 255, 0, 255 )
+			
+			local shootpos = self.Owner:GetShootPos()
+			local aim = self.Owner:GetAimVector():Angle()
+			local vup = Vector( 0, 0, 1 )
+			local vright = aim:Right()
+			
+			local vtls = vup * 72 - vright * 16
+			local vtlc = vup * 36 - vright * 16
+			local vbrs = vright * 16
+			local vbrc = vbrs
+			
+			local x = ScrW() / 2.0
+			local y = ScrH() / 2.0
+			
+			local targdist = 0
+			local newtarg = nil
+			
+			local lasttarg = false
+			
+			local curtarg = nil
+			if self:GetNWBool( "HasTarget" ) then
+				curtarg = self:GetNWEntity( "CurTarget" )
 			end
-			draw.SimpleText( text, "ScoreboardText", ScrW() / 2, ScrH() * 3 / 4, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
-		 else
-			draw.SimpleText( "NO AMMO", "ScoreboardText", ScrW() / 2, ScrH() * 3 / 4, Color( 255, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
-		 end
-      else
-         return self.BaseClass.DrawHUD(self)
-      end
-   end
-   
-   function SWEP:AdjustMouseSensitivity()
-      return (self:GetIronsights() and 0.2) or nil
-   end
+			
+			for _, targ in pairs( self.Targets ) do
+				local ply = targ.Player
+				local pos = ply:GetPos()
+				local tl, br
+				if ply:Crouching() then
+					tl = ( pos + vtlc ):ToScreen()
+					br = ( pos + vbrc ):ToScreen()
+				else
+					tl = ( pos + vtls ):ToScreen()
+					br = ( pos + vbrs ):ToScreen()
+				end
+				
+				tl.x = tl.x - 16
+				tl.y = tl.y - 16
+				br.x = br.x + 16
+				br.y = br.y + 16
+				
+				if tl.x < x and tl.y < y and br.x > x and br.y > y then
+					local dist = pos:Distance( shootpos )
+					if dist < targdist or targdist == 0 then
+						newtarg = ply
+						targdist = dist
+					end
+				end
+				
+				local width, height = br.x - tl.x, br.y - tl.y
+				
+				if curtarg == ply then
+					lasttarg = true
+					surface.SetDrawColor( 255, 0, 0, 255 )
+				elseif lasttarg then
+					lasttarg = false
+					surface.SetDrawColor( 0, 255, 0, 255 )
+				end
+				
+				surface.DrawOutlinedRect( tl.x, tl.y, width, height )
+			end
+			
+			if newtarg ~= self.CurTarget then
+				self:UpdateTarget( newtarg )
+			end
+		
+			surface.SetDrawColor( 0, 0, 0, 255 )
+
+			local scope_size = ScrH()
+
+			-- crosshair
+			local gap = 80
+			local length = scope_size
+			surface.DrawLine( x - length, y, x - gap, y )
+			surface.DrawLine( x + length, y, x + gap, y )
+			surface.DrawLine( x, y - length, x, y - gap )
+			surface.DrawLine( x, y + length, x, y + gap )
+
+			gap = 0
+			length = 50
+			surface.DrawLine( x - length, y, x - gap, y )
+			surface.DrawLine( x + length, y, x + gap, y )
+			surface.DrawLine( x, y - length, x, y - gap )
+			surface.DrawLine( x, y + length, x, y + gap )
+
+
+			-- cover edges
+			local sh = scope_size / 2
+			local w = (x - sh) + 2
+			surface.DrawRect(0, 0, w, scope_size)
+			surface.DrawRect(x + sh - 2, 0, w, scope_size)
+
+			surface.SetDrawColor(255, 0, 0, 255)
+			surface.DrawLine(x, y, x + 1, y + 1)
+
+			-- scope
+			surface.SetTexture(scope)
+			surface.SetDrawColor(255, 255, 255, 255)
+
+			surface.DrawTexturedRectRotated(x, y, scope_size, scope_size, 0)
+
+			surface.SetDrawColor(0, 0, 0, 255)
+			surface.DrawRect( ScrW() / 2 - 129, ScrH() * 3 / 4 - 9, 258, 18 )
+			if self:Clip1() ~= 0 then
+				local ctime = CurTime()
+				local r = 255
+				local gb = 0
+				if ctime >= self.ChargeEnd then
+					local wave = ( math.sin( ( CurTime() - self.ChargeEnd ) * math.pi * 2 ) * 0.5 + 0.5 )
+					r = wave * r + ( 1 - wave ) * 255
+					gb = wave * gb + ( 1 - wave ) * 255
+				end
+				surface.SetDrawColor(r, gb, gb, 255)
+				local prog = math.max( math.min( ( CurTime() - self.ChargeEnd + self.ChargeTime ) / self.ChargeTime, 1 ), 0 )
+				local len = math.floor( prog * 256 )
+				surface.DrawRect( ScrW() / 2 - 128, ScrH() * 3 / 4 - 8, len, 16 )
+
+				local text = "" .. math.floor( prog * 100 ) .. "%"
+				if prog == 1 then
+					text = "FULLY CHARGED"
+				end
+				draw.SimpleText( text, "ScoreboardText", ScrW() / 2, ScrH() * 3 / 4, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+			else
+				draw.SimpleText( "NO AMMO", "ScoreboardText", ScrW() / 2, ScrH() * 3 / 4, Color( 255, 0, 0, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+			end
+		else
+			return self.BaseClass.DrawHUD(self)
+		end
+	end
+
+	function SWEP:AdjustMouseSensitivity()
+		return (self:GetIronsights() and 0.2) or nil
+	end
 end
