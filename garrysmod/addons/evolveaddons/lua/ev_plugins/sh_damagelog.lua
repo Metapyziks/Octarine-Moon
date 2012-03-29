aded245323847a9cb0773f24d460b0855ac0ef7b
Metapyziks <metapyziks@gmail.com>
2012-03-29 17:34:38 +0100

    Added filters to the damagelog

--- garrysmod/addons/evolveaddons/lua/ev_plugins/sh_damagelog.lua
+++ garrysmod/addons/evolveaddons/lua/ev_plugins/sh_damagelog.lua
@@ -7,11 +7,12 @@ PLUGIN.Title = "Damagelog"
 PLUGIN.Description = "Outputs a damage log of the last round"
 PLUGIN.Author = "Metapyziks"
 PLUGIN.ChatCommand = "damagelog"
+PLUGIN.Usage = "[<all | ff> [player]]"
 
 if( SERVER ) then
 	function PLUGIN:Call( ply, args )
 		if ( not ValidEntity( ply ) ) or ply:IsSuperAdmin() or GetRoundState() != ROUND_ACTIVE then
-			ply:ConCommand( "ttt_print_damagelog" )
+			ply:ConCommand( "ttt_print_damagelog " .. table.concat( args, " " ) )
 			evolve:Notify( ply, evolve.colors.white, "Damage log printed in console." )
 		else
 			evolve:Notify( ply, evolve.colors.red, "You are not allowed to print the damage log until the round ends!" )
